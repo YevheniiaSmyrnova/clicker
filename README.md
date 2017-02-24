31629ce3fb9f7d0eafa7859722b66bc4e7ba2372
yevheniia <y.smyrnova@quantumobile.com>
2017-02-24 20:25:02 +0200

    add arguments

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ click on the found position.
 ## Usage
 
 The script has two required arguments: URL and path to the image.
+And five optional arguments: offset on x and y, proportional offset factor of the
+image on x and y, and delay before making screenshot
 
 ```
 git clone https://yevheniia1393@bitbucket.org/yevheniia1393/quantu.git
@@ -26,7 +28,7 @@ cd quantu/
 virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python main.py https://www.youtube.com/watch?v=l8F2g7I-qh0 pattern.png
+python main.py https://www.youtube.com/watch?v=l8F2g7I-qh0 pattern.png --default_x_offset=0 --default_y_offset=66 --proportion_img_x=0.5 --proportion_img_y=0.5 --delay=5
 ```
 
 ## Usage via docker
@@ -43,7 +45,7 @@ Run
 ```
 docker run -it --rm -p 5935:5935 -v `pwd`:/clicker clicker bash
 vncserver :35 -geometry 1400x1050
-python main.py https://www.youtube.com/watch?v=l8F2g7I-qh0 pattern.png
+python main.py https://www.youtube.com/watch?v=l8F2g7I-qh0 pattern.png --default_x_offset=0 --default_y_offset=66 --proportion_img_x=0.5 --proportion_img_y=0.5 --delay=5
 ```
 
 On machine with X11 start vncviewer and connect to IP of host running clicker and port 5935, for instance
--- main.py
+++ main.py
@@ -7,18 +7,12 @@ import argparse
 import logging
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("url", help="URL to the page")
-parser.add_argument("img", help="path to the image")
-args = parser.parse_args()
-
-
-def driver_click(driver, x, y, default_y_offset=66):
+def driver_click(driver, x, y, default_x_offset, default_y_offset):
 
     # more broot force click coords via X11 automation tool
     # this doesn't work with tightvnc though
     position = driver.get_window_position()
-    x += position['x']
+    x += position['x'] + default_x_offset
     # need to take into account offset by y that is a constant
     y += position['y'] + default_y_offset
     subprocess.call(["xdotool", "mousemove", "--sync", str(x), str(y), "click", "1"])
@@ -38,7 +32,7 @@ def get_screenshot(driver):
     return cv2.imread(temp_file.name)
 
 
-def get_coordinates_pattern(img_rgb, pattern):
+def get_coordinates_pattern(img_rgb, pattern, proportion_img_x, proportion_img_y):
     w, h = pattern.shape[:-1]
     res = cv2.matchTemplate(img_rgb, pattern, cv2.TM_CCOEFF_NORMED)
     threshold = .8
@@ -49,11 +43,11 @@ def get_coordinates_pattern(img_rgb, pattern):
     elif len(points) > 1:
         logging.warning("Several occurrences are found, using the first among them.")
     pt = points[0]
-    x, y = (pt[0] + w / 2.0, pt[1] + h / 2.0)
+    x, y = (pt[0] + proportion_img_x * w, pt[1] + proportion_img_y * h)
     return x, y
 
 
-def main(url, img):
+def main(url, img, default_x_offset, default_y_offset, proportion_img_x, proportion_img_y, delay):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--start-maximized")
@@ -61,15 +55,15 @@ def main(url, img):
     driver.get(url)
     img_rgb = get_screenshot(driver)
     pattern = cv2.imread(img)
-    coords = get_coordinates_pattern(img_rgb, pattern)
+    coords = get_coordinates_pattern(img_rgb, pattern, proportion_img_x, proportion_img_y)
     if coords is None:
         logging.error("Image isn't found.")
         return
-    driver_click(driver, coords[0], coords[1])
+    driver_click(driver, coords[0], coords[1], default_x_offset, default_y_offset)
 
     # give time for chrome to react
     import time
-    time.sleep(5)
+    time.sleep(delay)
 
     driver.get_screenshot_as_file("result.png")
     logging.info("Result saved to result.png")
@@ -80,4 +74,29 @@ def main(url, img):
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-    main(args.url, args.img)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL to the page")
+    parser.add_argument("img", help="path to the image")
+    parser.add_argument("--default_x_offset",
+                        help="offset on x",
+                        default=0,
+                        type=float)
+    parser.add_argument("--default_y_offset",
+                        help="offset on y",
+                        default=66,
+                        type=float)
+    parser.add_argument("--proportion_img_x",
+                        help="proportional offset factor of the image on x",
+                        default=0.5,
+                        type=float)
+    parser.add_argument("--proportion_img_y",
+                        help="proportional offset factor of the image on y",
+                        default=0.5,
+                        type=float)
+    parser.add_argument("--delay",
+                        help="how long it waits after performing the action",
+                        default=5,
+                        type=float)
+    args = parser.parse_args()
+    main(args.url, args.img, args.default_x_offset, args.default_y_offset,
+         args.proportion_img_x, args.proportion_img_y, args.delay)
